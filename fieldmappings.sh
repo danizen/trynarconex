e81b3343b4364f48cbda38e815c3f45bcaa8ba54
Dan Davis <daniel.davis@nih.gov>
2017-04-26 16:33:34 -0400

    Enhance utility scripts to speak to a different ES host.

--- fieldmappings.sh
+++ fieldmappings.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-curl -X PUT "http://localhost:9200/medlineplus/_mappings/page?pretty=true" --data @- <<EOF
+ES_HOST=localhost
+ES_PORT=9200
+ES_INDEX=medlineplus
+
+while getopts 'h:p:i' opt; do 
+  case $opt in
+    h) ES_HOST=$OPTARG ;;
+    p) ES_PORT=$OPTARG ;;
+    i) ES_INDEX=$OPTARG ;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+  esac
+done
+
+curl -X PUT "http://$ES_HOST:$ES_PORT/$ES_INDEX/_mappings/page?pretty=true" --data @- <<EOF
 {
   "properties": {
     "content": {
--- search.sh
+++ search.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-curl "http://localhost:9200/medlineplus/_search?pretty=true&q=*:*" 
+ES_HOST=localhost
+ES_PORT=9200
+ES_INDEX=medlineplus
+
+while getopts 'h:p:i' opt; do 
+  case $opt in
+    h) ES_HOST=$OPTARG ;;
+    p) ES_PORT=$OPTARG ;;
+    i) ES_INDEX=$OPTARG ;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+  esac
+done
+
+curl "http://$ES_HOST:$ES_PORT/$ES_INDEX/_search?pretty=true&q=*:*" 
 
