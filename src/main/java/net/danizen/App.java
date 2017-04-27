c9f42b649b157b4bbfd97768d0e13cb6767435f6
Daniel Davis <dan@danizen.net>
2017-04-26 22:43:44 -0400

    Getting closer to complete crawler

--- src/main/java/net/danizen/App.java
+++ src/main/java/net/danizen/App.java
@@ -21,45 +21,44 @@ public class App
 {
     public static void main( String[] args )
     {
-    		Options options = new Options();
-    		options.addOption("c", true, "Path to narconex XML version");
-    		options.addOption("v", true, "Path to variables");
+   		Options options = new Options();
+   		options.addOption("c", true, "Path to narconex XML version");
+   		options.addOption("v", true, "Path to variables");
 
-    		CommandLineParser parser = new DefaultParser();
-    		CommandLine cmd = null;
-    		try { 
-						cmd = parser.parse(options, args);
-    		} catch (ParseException e) {
-    				System.err.println("error: cannot parse errors");
-    				System.exit(1);
-    		}
+   		CommandLineParser parser = new DefaultParser();
+   		CommandLine cmd = null;
+   		try { 
+			cmd = parser.parse(options, args);
+  		} catch (ParseException e) {
+			System.err.println("error: cannot parse errors");
+			System.exit(1);
+   		}
     		
+   		if (cmd.hasOption("c") == false) {
+			System.err.println("error: -c argument required");
+			System.exit(1);
+   		}
+   		File xmlFile = new File(cmd.getOptionValue("c"));
 
-    		if (cmd.hasOption("c") == false) {
-    				System.err.println("error: -c argument required");
-    				System.exit(1);
-    		}
-    		File xmlFile = new File(cmd.getOptionValue("c"));
-
-    		File varsFile = null;
-    		if (cmd.hasOption("v") == true) {
-    				varsFile = new File(cmd.getOptionValue("v"));
-    		}
+   		File varsFile = null;
+   		if (cmd.hasOption("v") == true) {
+			varsFile = new File(cmd.getOptionValue("v"));
+   		}
     		
         HttpCollectorConfig config = null;
-				try {
-        		config = (HttpCollectorConfig) new CollectorConfigLoader(HttpCollectorConfig.class)
-        																			.loadCollectorConfig(xmlFile, varsFile);
+		try {
+       		config = (HttpCollectorConfig) new CollectorConfigLoader(HttpCollectorConfig.class)
+                .loadCollectorConfig(xmlFile, varsFile);
         } catch (IOException e) {
-        		System.err.println("error: I/O Exception");
-        		System.exit(1);
+        	System.err.println("error: I/O Exception");
+        	System.exit(1);
         }
         if (config == null) {
-	        	System.err.println("error: config has failed");
-	        	System.exit(1);
+	        System.err.println("error: config has failed");
+	        System.exit(1);
         }
-     		config.setId("myid");
-     		config.setLogsDir("logs");
+     	config.setId("myid");
+     	config.setLogsDir("logs");
         
         HttpCollector collector = new HttpCollector(config);
         collector.start(true);
